09659c615e2103065d2c21cbe1019737fa11d5ab
Travis Bumgarner <travis-bumgarner@ip-192-168-82-55.us-west-2.compute.internal>
2018-06-26 17:14:00 -0400

    Modified load script for proper variable types

--- load_data.py
+++ load_data.py
@@ -9,12 +9,26 @@ def load_data(index, doc_type, file):
     with open(file) as f:
         reader = csv.DictReader(f)
 
-        headers = next(reader)
-        print(headers)
+        headers = ["street", "city", "zip", "state", "beds", "sq__ft", "sale_date"]
+
 
         for line in reader:
-            es.index(index=index, doc_type=doc_type, body=line)
-            print(line)
-load_data('people2', 'person', './us-500.csv')
+            street, city, zip, state, beds, sq__ft, sale_date = line.values()
 
-# "LatD", "LatM", "LatS", "NS", "LonD", "LonM", "LonS", "EW", "City", "State"
+            data = {
+                "street": street,
+                "city": city,
+                "zip": zip,
+                "state": state,
+                "beds": int(beds),
+                "sq__ft": int(sq__ft),
+                "sale_date": sale_date
+            }
+
+            es.index(index="housing4", doc_type="house", body=data)
+
+load_data('housing4', 'house', './housing.csv')
+
+# LatD", "LatM", "LatS", "NS", "LonD", "LonM", "LonS", "EW", "City", "State"
+
+
